f21f402d49eb5e9db107942e3f5c7e7da04f9119
Alan Freire <alan_freire@msn.com>
2017-04-02 11:27:42 -0300

    Update CRUD Quadras

--- src/app/View/template/footer.php
+++ src/app/View/template/footer.php
@@ -9,5 +9,14 @@
         </footer>
         <script src="public/js/jquery-3.1.1.min.js"></script>
         <script src="public/js/bootstrap.min.js"></script>
+        <script>
+            $(document).on('click', '.delete', function(event) {
+                event.preventDefault();
+                result = confirm('Deseja realmente apagar o registro?');
+                if (result) {
+                    window.location.href = $(this).attr('href');
+                }
+            });
+        </script>
     </body>
 </html>
