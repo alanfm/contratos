7f0aa0ee5146ead18578c725603b91fe10fe7896
Alan Freire <alan_freire@msn.com>
2017-06-23 17:30:24 -0300

    Uso de fontes local

--- src/app/View/template/header.php
+++ src/app/View/template/header.php
@@ -7,8 +7,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="public/css/bootstrap.min.css">
         <link rel="stylesheet" href="public/css/font-awesome.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
         <style type="text/css">
+            /* latin-ext */
+            @font-face {
+                font-family: 'Dosis';
+                font-style: normal;
+                font-weight: 400;
+                src: local('Dosis Regular'), local('Dosis-Regular'), url(public/fonts/RlBXAIuiO5GvH9-0-JbBlw.woff2) format('woff2');
+                unicode-range: U+0100-024F, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF;
+            }
+            /* latin */
+            @font-face {
+                font-family: 'Dosis';
+                font-style: normal;
+                font-weight: 400;
+                src: local('Dosis Regular'), local('Dosis-Regular'), url(public/fonts/4hYyXH_8WmbBLamf6WjLwg.woff2) format('woff2');
+                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;
+            }
             html {
                 position: relative;
                 min-height: 100%;
